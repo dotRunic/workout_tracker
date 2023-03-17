e6392edfd9ec61672c5ee2e3817b053f6e8e4457
GitMichaGit <mbeyer98@yahoo.com>
2023-03-17 15:49:28 +0100

    id fix

--- WorkoutTracker.java
+++ WorkoutTracker.java
@@ -9,10 +9,10 @@ public class WorkoutTracker {
         List<Workout> workouts = new ArrayList<>();
         people.forEach(p -> {
             long id = 0;
-            workouts.add(new BikingWorkout(id++, 1, 15, p.getId(), BikingType.MOUNTAIN));
-            workouts.add(new BikingWorkout(id++, 5, 20, p.getId(), BikingType.ROAD));
-            workouts.add(new SwimmingWorkout(id++, 10, 25, p.getId(), SwimmingType.BACKSTROKE));
-            workouts.add(new SwimmingWorkout(id++, 15, 30, p.getId(), SwimmingType.BUTTERFLY));
+            workouts.add(new BikingWorkout(id, 5, 15, id++, BikingType.MOUNTAIN));
+            workouts.add(new BikingWorkout(id, 5, 20, id++, BikingType.ROAD));
+            workouts.add(new SwimmingWorkout(id, 10, 25, id++, SwimmingType.BACKSTROKE));
+            workouts.add(new SwimmingWorkout(id, 15, 30, id++, SwimmingType.BUTTERFLY));
         });
 
         FileUtils.writeStatisticToFile(people, workouts);
