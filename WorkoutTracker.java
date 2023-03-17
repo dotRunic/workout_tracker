af3ad8bc64a5857fecf2b718c275b31771ae3a87
GitMichaGit <mbeyer98@yahoo.com>
2023-03-17 13:58:40 +0100

    distance change

--- WorkoutTracker.java
+++ WorkoutTracker.java
@@ -9,10 +9,10 @@ public class WorkoutTracker {
         List<Workout> workouts = new ArrayList<>();
         people.forEach(p -> {
             long id = 0;
-            workouts.add(new BikingWorkout(id++, 10, 100, p.getId(), BikingType.MOUNTAIN));
-            workouts.add(new BikingWorkout(id++, 20, 100, p.getId(), BikingType.ROAD));
-            workouts.add(new SwimmingWorkout(id++, 30, 100, p.getId(), SwimmingType.BACKSTROKE));
-            workouts.add(new SwimmingWorkout(id++, 40, 100, p.getId(), SwimmingType.BUTTERFLY));
+            workouts.add(new BikingWorkout(id++, 20, 100, p.getId(), BikingType.MOUNTAIN));
+            workouts.add(new BikingWorkout(id++, 40, 100, p.getId(), BikingType.ROAD));
+            workouts.add(new SwimmingWorkout(id++, 60, 100, p.getId(), SwimmingType.BACKSTROKE));
+            workouts.add(new SwimmingWorkout(id++, 80, 100, p.getId(), SwimmingType.BUTTERFLY));
         });
 
         FileUtils.writeStatisticToFile(people, workouts);
